a9f369af97d78f491e96bb45b61efa4cc2b41cb8
unknown <ryanjude.miranda@gmail.com>
2020-07-25 16:01:44 +0530

    Prepared for deployment

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const cors = require("cors");
 const AppError = require("./utils/appError");
 const bodyParser = require("body-parser");
+const path = require("path")
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 
 const authRoute = require("./routes/authRoute");
 app.use("/api/users", authRoute);
@@ -15,10 +17,18 @@ app.use("/api/users", authRoute);
 const pageRoute = require("./routes/pageRoute");
 app.use("/api/pages", pageRoute);
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 // Unhandled routes
-app.all("*", (req, res, next) => {
-  return next(new AppError("Route not found", 404));
-});
+// app.all("*", (req, res, next) => {
+//   return next(new AppError("Route not found", 404));
+// });
 
 // Global error handler
 const globalErrorHandler = require("./utils/globalErrorHandler");
