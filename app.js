1955996b4d0c3bf2bf314b8fb2fc62f6a5355c6d
unknown <ryanjude.miranda@gmail.com>
2020-07-25 16:27:05 +0530

    deployment issue #2

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const app = express();
 const cors = require("cors");
 const AppError = require("./utils/appError");
 const bodyParser = require("body-parser");
-const path = require("path")
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 const authRoute = require("./routes/authRoute");
 app.use("/api/users", authRoute);
@@ -21,8 +20,8 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+    res.sendFile(path.resolve(__dirname, "client", "build"));
+  });
 }
 
 // Unhandled routes
